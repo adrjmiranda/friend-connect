b6ea11f029c18cf699c43e55ddfb75ca2368c3af
Adriano Miranda <adrjmiranda@gmail.com>
2024-03-24 18:02:45 -0300

    finished.

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,10 @@ import GlobalStyles from './GlobalStyles.ts';
 // Import Pages
 import App from './App.tsx';
 import Home from './pages/home/index.tsx';
-import Login from './pages/login/index.tsx';
-import Register from './pages/register/index.tsx';
-import Profile from './pages/profile/index.tsx';
-import UserProfile from './pages/user-profile/index.tsx';
+// import Login from './pages/login/index.tsx';
+// import Register from './pages/register/index.tsx';
+// import Profile from './pages/profile/index.tsx';
+// import UserProfile from './pages/user-profile/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<BrowserRouter>
@@ -18,13 +18,13 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 		<Routes>
 			<Route element={<App />}>
 				<Route path='/friend-connect' element={<Home />} />
-				<Route path='/friend-connect/login' element={<Login />} />
+				{/* <Route path='/friend-connect/login' element={<Login />} />
 				<Route path='/friend-connect/register' element={<Register />} />
 				<Route path='/friend-connect/profile' element={<Profile />} />
 				<Route
 					path='/friend-connect/user-profile/:id'
 					element={<UserProfile />}
-				/>
+				/> */}
 			</Route>
 		</Routes>
 	</BrowserRouter>
