eb916eb22c9a2dff141328512e05e1dcedaee422
Adriano Miranda <adrjmiranda@gmail.com>
2024-03-24 17:45:08 -0300

    chore: added advertising component.

--- src/components/advertising/index.tsx
+++ src/components/advertising/index.tsx
@@ -1,11 +1,25 @@
 import { FunctionComponent, ReactNode } from 'react';
 
+// Styles
 import * as Style from './style';
+
+// Images
+import AdvertisingImg from '../../assets/img/add.jpg';
+import GiftImg from '../../assets/img/gift.png';
 
 const Advertising: FunctionComponent = (): ReactNode => {
 	return (
 		<Style.Container>
-			<h1>Advertising</h1>
+			<div className='birthdays'>
+				<img src={GiftImg} alt='' />
+				<p>
+					<span>Jane Smith</span> and <span>5 others</span> have a birthday
+					tomorrow
+				</p>
+			</div>
+			<div className='add'>
+				<img src={AdvertisingImg} alt='' />
+			</div>
 		</Style.Container>
 	);
 };
--- src/components/advertising/style.ts
+++ src/components/advertising/style.ts
@@ -1,3 +1,26 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	.birthdays {
+		display: flex;
+		align-items: center;
+		color: var(--sextenary-color);
+		gap: 1rem;
+		font-weight: 400;
+		margin-bottom: 2rem;
+	}
+
+	.birthdays img {
+		height: 80px;
+	}
+
+	.birthdays span {
+		color: var(--septenary-color);
+		font-weight: bold;
+	}
+
+	.add img {
+		width: 100%;
+		border-radius: 5px;
+	}
+`;
